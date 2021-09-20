b9978d34244da3f910309a834b229ad7f3d48926
Riya2807 <58070197+Riya2807@users.noreply.github.com>
2021-09-20 23:07:23 +0530

    Delete RunnerUpScore.py

--- RunnerUpScore.py
+++
@@ -1,7 +0,0 @@
-#Takes n(number of elements in array) and arr(array elements) as inputs and produces the second largest number from the input array
-n = int(input())
-arr = map(int, input().split())
-new_arr = set(arr)
-m=max(new_arr)
-new_arr.remove(m)
-print(max(new_arr))
